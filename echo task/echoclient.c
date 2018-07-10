2b0e6ae2c9c7f25c30b7bba89f8ec25e315664f0
AndyStep <stepanoff.work@gmail.com>
2018-07-10 19:15:19 +0700

    serv-works,cli-doesn't

--- echo task/echoclient.c
+++ echo task/echoclient.c
@@ -2,32 +2,39 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <stdio.h>
-#include<string.h>
-
-#define INADDR_ANY_INIT {{ 0,0,0,0 }}
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
 	int sockfd;
-	struct sockaddr_in addr;
-	const struct in_addr in_addr_any = INADDR_ANY_INIT;
+	struct sockaddr_in servaddr,cliaddr;
 	const char *ip = "127.0.0.1";
-	const char *sendbuf = "I<3U"; 
-	char recievebuf[100];
 	
-	memset(&addr, 0, sizeof(sturct sockaddr_in));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htons(ip);
-	addr.sin_port = htons(22000); 
+	const char *sendbuf = "I<3U"; // Отправляемое сообщ.
+	char recievebuf[100];			//Буфер для приема сообщ.
+
+	////////////////////////////////////////////////
+	int servaddr_len = sizeof(servaddr);
+	int cliaddr_len  = sizeof(cliaddr);
+	int sendbuf_len    = 5;
+	int recievebuf_len = sizeof(recievebuf);
+	int sockadrr_in_sz = sizeof(struct sockaddr_in);
+	////////////////////////////////////////////////
+
+	memset(&servaddr, 0, sockadrr_in_sz);
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr(ip); //
+	servaddr.sin_port = htons(22000); 
 
 	sockfd = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
 	
-	sendto(sockfd,sendbuf,sizeof(sendbuf), 0,			//[not filled]
-				addr, sizeof(addr));
+	sendto(sockfd,sendbuf,sendbuf_len, 0, (struct sockaddr*)&servaddr, servaddr_len);
 
-	recvfrom(sockfd, recievebuf, sizoef(recievebuf), 0,
-				addr, sizeof(addr));
-	
+	recvfrom(sockfd, recievebuf, recievebuf_len, 0,(struct sockaddr*)&cliaddr, &cliaddr_len);
+	printf("%s\n", recievebuf);
 	close(sockfd);
 	return 0;
 }
--- echo task/echoserver.c
+++ echo task/echoserver.c
@@ -2,31 +2,41 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <stdio.h>
-#include<string.h>
-
-#define INADDR_ANY_INIT {{ 0,0,0,0 }}
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
 	int sockfd;
-	struct sockaddr_in addr;
-	const struct in_addr in_addr_any = INADDR_ANY_INIT;
 	const char *ip = "127.0.0.1";
+
+	struct sockaddr_in servaddr, cliaddr;
 	char recievebuf[100];
+
+	////////////////////////////////////////////////
+	int servaddr_len = sizeof(servaddr);
+	int cliaddr_len  = sizeof(cliaddr);
+	int recievebuf_len = 5;
+	int sockadrr_in_sz = sizeof(struct sockaddr_in);
+	////////////////////////////////////////////////
 	
-	memset(&addr, 0, sizeof(sturct sockaddr_in));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htons(ip);
-	addr.sin_port = htons(22000); 
+	memset(&servaddr, 0, sockadrr_in_sz);
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr(ip);
+	servaddr.sin_port = htons(22000); 
 
 	sockfd = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
-	bind(sockfd,(struct sockaddr_in *)&addr,sizeof(addr));
+	bind(sockfd,(struct sockaddr *)&servaddr,servaddr_len);
 
-	recvfrom(sockfd, recievebuf, sizoef(recievebuf), 0,
-				addr, sizeof(addr));
+	/* Получить сообщение от клиента */
+	recvfrom(sockfd, recievebuf, recievebuf_len, 0,	(struct sockaddr *)&cliaddr, &cliaddr_len);
+	printf("%s\n",recievebuf);
+	/*	Изменю и отправлю клиенту полученное сообщение */
+	recievebuf[3]='K';
 
-	sendto(sockfd,recievebuf,sizoef(recievebuf), 0,			//[not filled]
-				addr, sizeof(addr));
+	sendto(sockfd,recievebuf,recievebuf_len, 0, (struct sockaddr *)&servaddr, servaddr_len);
 	close(sockfd);
 	return 0;
 }
